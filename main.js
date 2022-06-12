e2e9aa9c290e19ebfc02ad1ed7cdecc85dd40c12
prajwal <prajwalfarkunde72@gmail.com>
2022-06-12 16:37:49 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click',function(){
+    let r = Math.floor(Math.random()*256);
+    let g =Math.floor(Math.random()*256);
+    let b =Math.floor(Math.random()*256);
+    let a = Math.random();
+
+    // alert(r+" "+g+" "+b);
+    let color = 'rgba('+r+","+g+","+b+","+a+')';
+    alert(color);
+    document.body.style.backgroundColor = 'rgba('+r+","+g+","+b+","+a+')';
+  btn.style.color = 'rgb('+r+","+g+","+b+')';
+
+}); 
+
