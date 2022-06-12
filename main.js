6a6c5a72019ea70678d4a80c145d4ef931778dd4
prajwal <prajwalfarkunde72@gmail.com>
2022-06-12 17:55:47 +0530

    done

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ btn.addEventListener('click',function(){
     let b =Math.floor(Math.random()*256);
     let a = Math.random();
 
-    // alert(r+" "+g+" "+b);
+
     let color = 'rgba('+r+","+g+","+b+","+a+')';
-    alert(color);
+
     document.body.style.backgroundColor = 'rgba('+r+","+g+","+b+","+a+')';
-  btn.style.color = 'rgb('+r+","+g+","+b+')';
+
 
 }); 
 
